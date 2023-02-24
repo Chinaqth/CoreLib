aa483f19c88cc2a7a6c8a1c2c0a90c7a48c13321
qiutianhao <Chinaqth@gmail.com>
2023-02-24 16:51:39 +0800

    [lib] WindowUtil statusBar color (light dark mod

--- corelib/src/main/java/com/corelib/utils/WindowUtil.kt
+++ corelib/src/main/java/com/corelib/utils/WindowUtil.kt
@@ -1,11 +1,17 @@
 package com.corelib.utils
 
 import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Build
+import android.util.Log
 import android.view.View
+import android.view.ViewGroup
 import android.view.WindowInsets
 import android.view.WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+import android.view.WindowManager
 import com.corelib.activity.CoreActivity
+import kotlin.math.abs
+
 
 /**
  *  # WindowUtil
@@ -77,12 +83,29 @@ object WindowUtil {
      */
     fun fullScreenMode(activity: CoreActivity) {
         activity.window.let {
+            //状态栏透明
             it.statusBarColor = Color.TRANSPARENT
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
                 it.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                it.decorView.windowInsetsController?.setSystemBarsAppearance(APPEARANCE_LIGHT_STATUS_BARS, APPEARANCE_LIGHT_STATUS_BARS)
+                val color = (it.decorView.findViewById<ViewGroup>(android.R.id.content)
+                    .getChildAt(0).background as ColorDrawable).color
+                if (!isLightColor(abs(color))) {
+                    it.decorView.windowInsetsController?.setSystemBarsAppearance(APPEARANCE_LIGHT_STATUS_BARS, APPEARANCE_LIGHT_STATUS_BARS)
+                } else {
+                    it.decorView.windowInsetsController?.setSystemBarsAppearance(0, APPEARANCE_LIGHT_STATUS_BARS)
+                }
             }
         }
+    }
+
+    /**
+     * 计算颜色
+     */
+    private fun isLightColor(color : Int) : Boolean{
+        // 计算颜色的相对亮度
+        val brightness = (299 * Color.red(color) + 587 * Color.green(color) + 114 * Color.blue(color)) / 1000.0;
+        // 如果颜色的相对亮度大于等于 128，则认为是浅色
+        return brightness >= 128;
     }
 
 }
